560187932d508bf12eabeac4c87cb861996e5368
mszawerd <01169246@pw.edu.pl>
2023-12-10 08:54:37 +0100

    init raport generator

---
+++ raport-generator/src/test/java/com/parcel/raportgenerator/TestRaportGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.parcel.raportgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestRaportGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(RaportGeneratorApplication::main).with(TestRaportGeneratorApplication.class).run(args);
+	}
+
+}
