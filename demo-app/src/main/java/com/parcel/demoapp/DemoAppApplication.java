95a53b7142addec5d63a3eb59af89b0919cac1b8
Marcin Jarczewski <m.jarczewski@stud.elka.pw.edu.pl>
2023-12-11 01:15:32 +0100

    feat: add api gateway (!8)

--- demo-app/src/main/java/com/parcel/demoapp/DemoAppApplication.java
+++ demo-app/src/main/java/com/parcel/demoapp/DemoAppApplication.java
@@ -7,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,7 +32,11 @@ public class DemoAppApplication {
 
 	@RequestMapping("/test")
 	public String greeting() {
-		return String.format("Demo app: Hello from '%s'!", greetingClient.greeting());
+		try {
+			return String.format("Demo app: Hello from '%s'!", greetingClient.greeting());
+		} catch (Exception e) {
+			return String.format("Error occured: %s", e.toString());
+		}
 	}
 
 	public static void main(String[] args) {
