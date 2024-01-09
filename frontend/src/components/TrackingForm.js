e42f15e41688fb4652dc897d06224e9f28c5f6a0
jjazdzyk <01169177@pw.edu.pl>
2024-01-09 10:13:02 +0100

    Frontend

---
+++ frontend/src/components/TrackingForm.js
@@ -0,0 +1,45 @@
+// components/TrackingForm.js
+import React, { useState } from 'react';
+import './TrackingForm.css';
+import axios from 'axios';
+
+const TrackingForm = ({ onTrack }) => {
+    const [trackingNumber, setTrackingNumber] = useState('');
+    const [carrier, setCarrier] = useState('');
+
+    const handleTrack = async () => {
+        try {
+            const response = await axios.post('http://localhost:9090/tracking/add-parcel', {
+                id: trackingNumber,
+                carrierName: carrier,
+            });
+
+            console.log('Pomyślnie dodano paczkę!', response.data);
+            onTrack({ trackingNumber, carrier });
+        } catch (error) {
+            console.error('Błąd podczas dodawania paczki.', error);
+        }
+    };
+
+    return (
+        <div className="tracking-form">
+            <h2>Nadaj Paczkę</h2>
+            <label>Numer Przesyłki:</label>
+            <input
+                type="text"
+                value={trackingNumber}
+                onChange={(e) => setTrackingNumber(e.target.value)}
+            />
+            <label>Przewoźnik:</label>
+            <select value={carrier} onChange={(e) => setCarrier(e.target.value)}>
+                <option value="">Wybierz przewoźnika</option>
+                <option value="ship24">Ship24</option>
+                <option value="inpost">InPost</option>
+                <option value="fedex">FedEx</option>
+            </select>
+            <button onClick={handleTrack}>Nadaj Paczkę</button>
+        </div>
+    );
+};
+
+export default TrackingForm;
--- parcel-tracker/src/main/java/com/parcel/tracker/TrackingController.java
+++ parcel-tracker/src/main/java/com/parcel/tracker/TrackingController.java
@@ -2,10 +2,7 @@ package com.parcel.tracker;
 
 import com.parcel.tracker.model.ParcelRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/tracking")
@@ -18,6 +15,7 @@ public class TrackingController {
         this.trackerService = trackerService;
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/add-parcel")
     public void addParcelToTracking(@RequestBody ParcelRequest parcelRequest) {
         trackerService.addParcel(parcelRequest);
