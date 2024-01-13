02dc2734099e49b7553c29bd9eac16b9b5394f39
dkaszynski <01169181@pw.edu.pl>
2024-01-13 23:33:42 +0100

    Improve full text search query

--- parcel-finder/src/main/java/com/parcel/parcelfinder/infrastructure/elasticsearch/SpringDataElasticParcelRepository.java
+++ parcel-finder/src/main/java/com/parcel/parcelfinder/infrastructure/elasticsearch/SpringDataElasticParcelRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 import java.util.List;
 
 public interface SpringDataElasticParcelRepository extends ElasticsearchRepository<ElasticParcel, String> {
-    @Query("{\"multi_match\": {\"query\": \"?0\", \"fields\": [\"id\", \"carrier\", \"statusHistory\"], \"fuzziness\": \"AUTO\"}}")
+    @Query("{\"multi_match\": {\"query\": \"?0\", \"fields\": [\"id\", \"carrier\", \"statusHistory.status\"], \"fuzziness\": \"AUTO\"}}")
     List<ElasticParcel> find(String query);
 }
