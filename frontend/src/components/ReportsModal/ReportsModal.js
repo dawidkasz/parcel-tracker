bfc5aa2f489351efd3267f0344de3d30e0290d6b
dkaszynski <01169181@pw.edu.pl>
2024-01-15 19:32:28 +0100

    Pass ui id to download report

--- frontend/src/components/ReportsModal/ReportsModal.js
+++ frontend/src/components/ReportsModal/ReportsModal.js
@@ -31,7 +31,7 @@ const ReportsModal = ({ isVisible, onClose }) => {
                     <div className="modal-content">
                         <ul className="package-list-container">
                             {reports[0] ? reports.map((rep) => (
-                                <li key={rep.id} onClick={() => handleDownloadReport(rep)}>
+                                <li key={rep.id} onClick={() => handleDownloadReport(rep.id)}>
                                     <strong>Numer raportu:</strong> {rep.id}
                                     <strong>Query raportu:</strong> {rep.query}
                                     <strong>Czas stworzenia:</strong> {rep.creationTime}
