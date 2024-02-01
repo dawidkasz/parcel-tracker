8691a6580e7d35579b2374f6a7381755aafcf7be
Marcin Jarczewski <marcin.jarc@gmail.com>
2024-02-01 19:36:26 +0100

    fix: cleanup

--- test-service/src/main/java/com/parcel/testservice/GreetingController.java
+++
@@ -1,8 +0,0 @@
-package com.parcel.testservice;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-
-public interface GreetingController {
-    @RequestMapping("/")
-    String greeting();
-}
